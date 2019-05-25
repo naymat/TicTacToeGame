72dbf3c1aba7044357fa48db89f6dae660624300
naymat <naymat30@gmail.com>
2019-05-25 06:11:26 -0400

    Updated to newer create-react-app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./menu.css";
+import "./animate.css";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import GameBoard from "./components/board";
+import Menu from "./components/menue";
+import Board from "./games/Board";
+// const Board = require("./games/Board");
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let board = new Board();
+ReactDOM.render(
+  <div className="main">
+    <Menu />
+    <GameBoard board={board} />
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
